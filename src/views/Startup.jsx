47701d6b85a2e17452ef00cada395a926af36ec7
AmmarAhmed007 <ammar.ahmed@no.experis.com>
2022-02-24 00:08:09 +0100

    FEAT: Added router-dom and set routes for the different views

---
+++ src/views/Startup.jsx
@@ -0,0 +1,7 @@
+const Startup = () => {
+    return (
+        <h2>Start page</h2>
+    )
+}
+
+export default Startup
---
+++ src/views/Translation.jsx
@@ -0,0 +1,7 @@
+const Translation = () => {
+    return (
+        <h2>Translation page</h2>
+    )
+}
+
+export default Translation
