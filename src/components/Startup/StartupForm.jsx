f9387c95bbfa830ef72a363cb4f57d9f6524a611
AmmarAhmed007 <ammar.ahmed@no.experis.com>
2022-02-25 17:00:50 +0100

    Feat: Created user and app context and navigated user to translation page on successful login

--- src/components/Startup/StartupForm.jsx
+++ src/components/Startup/StartupForm.jsx
@@ -1,7 +1,10 @@
 import { useForm } from 'react-hook-form'
 import '../../App.css'
 import { loginUser } from '../../api/user'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { storageSave } from '../../utils/storage'
+import { useNavigate } from 'react-router-dom'
+import { useUser } from '../../context/UserContext'
 
 const usernameConfig = {
     required: true,
@@ -10,15 +13,28 @@ const usernameConfig = {
 
 const StartupForm = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const { user, setUser } = useUser()
+    const navigate = useNavigate()
 
     const [ loading, setLoading ] = useState(false);
     const [ apiError, setApiError ] = useState(null);
 
+    useEffect(() => {
+        if(user !== null) {
+            navigate('/translation')
+        }
+    }, [ user, navigate ])
+
     const onSubmit = async ({username}) => {
         setLoading(true);
-        const [ error, user] = await loginUser(username);
-        console.log('Error: ', error);
-        console.log('User: ', user);
+        const [ error, userResponse ] = await loginUser(username);
+        if(error !== null) {
+            setApiError(error)
+        }
+        if(userResponse !== null) {
+            storageSave('translation-user', userResponse)
+            setUser(userResponse)
+        }
         setLoading(false);
     }
     // console.log(errors);
@@ -52,7 +68,8 @@ return (
 
             {/* button is disabled if loading = true */}
             <button type="submit" disabled={loading}>Continue to translation</button>
-            { loading && <p>One second, logging in...</p>}
+            { loading && <p>One second, logging in...</p>} 
+            { apiError && <p>{ apiError } </p>}
         </form>
     </>
 )
