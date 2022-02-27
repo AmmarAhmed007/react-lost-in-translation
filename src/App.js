5a485816cd021af5d31edf81cba7710d08bcff60
AmmarAhmed007 <ammar.ahmed@no.experis.com>
2022-02-27 15:38:18 +0100

    Feat: Implemented logout functionality and dummy navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Profile from './views/Profile';
 import Startup from './views/Startup';
 import Translation from './views/Translation';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={ <Startup /> } />
           <Route path="/translation" element= { <Translation /> } />
--- src/components/Profile/ProfileActions.jsx
+++ src/components/Profile/ProfileActions.jsx
@@ -1,11 +1,25 @@
 import { Link } from "react-router-dom"
+import { STORAGE_KEY_USER } from "../../const/storageKeys"
+import { useUser } from "../../context/UserContext"
+import { storageDelete } from "../../utils/storage"
+
 
 const ProfileActions = () => {
+
+    const { setUser } = useUser()
+
+    const logOutClick = () => {
+        if (window.confirm('Are you sure you want to log out?')) {
+            storageDelete(STORAGE_KEY_USER)
+            setUser(null)
+        }
+    }
+
     return (
         <ul>
             <li><Link to="/translation">Translations</Link></li>
             <li><button>Clear history</button></li>
-            <li><button>Log out</button></li>
+            <li><button onClick={logOutClick}>Log out</button></li>
         </ul>
     )
 }
